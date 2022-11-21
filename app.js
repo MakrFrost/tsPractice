1265e51dbcbfed66796ba5c787fd43fabbf232ac
MakrFrost <frostmark@ukr.net>
2022-11-21 19:43:57 +0200

    Upd

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
 function add(num1, num2) {
     return num1 + num2;
 }
-add(1, 1);
+button.addEventListener("click", function () {
+    console.log(add(+input1.value, +input2.value));
+});
