487bca79f0caf03be36d12e52067304983eaf11e
MakrFrost <frostmark@ukr.net>
2022-12-05 20:56:30 +0200

    upd

--- src/2.ts
+++ src/2.ts
@@ -71,31 +71,31 @@
 
 //!
 
-type Page = {
-  title: string;
-  likes: number;
-  accounts: string[];
-  status: "open" | "close";
-  details?: {
-    createAt: string;
-    updateAt: string;
-  };
-};
+// type Page = {
+//   title: string;
+//   likes: number;
+//   accounts: string[];
+//   status: "open" | "close";
+//   details?: {
+//     createAt: string;
+//     updateAt: string;
+//   };
+// };
 
-const page1 = {
-  title: "The awesome page",
-  likes: 100,
-  accounts: ["Max", "Anton", "Nikita"],
-  status: "open",
-  details: {
-    createAt: "2021-01-01",
-    updateAt: "2021-05-01",
-  },
-};
+// const page1 = {
+//   title: "The awesome page",
+//   likes: 100,
+//   accounts: ["Max", "Anton", "Nikita"],
+//   status: "open",
+//   details: {
+//     createAt: "2021-01-01",
+//     updateAt: "2021-05-01",
+//   },
+// };
 
-const page2 = {
-  title: "Python or Js",
-  likes: 5,
-  accounts: ["Alex"],
-  status: "close",
-};
+// const page2 = {
+//   title: "Python or Js",
+//   likes: 5,
+//   accounts: ["Alex"],
+//   status: "close",
+// };
